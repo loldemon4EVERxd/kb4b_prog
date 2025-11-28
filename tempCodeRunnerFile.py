ab80777c0190084b5684c1863377d7007e462571
oparizek <lolondrej06@gmail.com>
2025-11-28 11:39:47 +0100

    2811

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-def hod_dvema_kostkami():
-    a=0
-    b=0
-    pokusy = 0
-    while True:
-        pokusy += 1
-        a = random.randint(1, 6)
-        b = random.randint(1, 6)
-        print(a, b)
-        if a == b and a == 2:
-            break
-    print(f"pocet hodu bylo {pokusy}")
+import matplotlib.pyplot as plt
