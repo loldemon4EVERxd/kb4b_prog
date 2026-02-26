00b795002f1d140d3d9439390d82e946dd7bbea7
loldemon4EVERxd <lolondrej06@gmail.com>
2026-02-26 16:44:11 +0100

    repair

--- NEURONKA UKOL FINAL.py
+++ NEURONKA UKOL FINAL.py
@@ -115,31 +115,12 @@ test_Y = Y[split:]
 # ---------- Neuronová síť ----------
 neural_network = MLPClassifier(
     hidden_layer_sizes=(32, 16, 8, 4),
-<<<<<<< HEAD
-<<<<<<< HEAD
     activation="logistic",
     max_iter=400,
     verbose=True
 )
 
-
 test_number = 1
-=======
-    activation="relu",
-=======
-    activation="logistic",
->>>>>>> a086f8f (neuronka final final final)
-    max_iter=400,
-    verbose=True
-)
-
-
-<<<<<<< HEAD
-test_number = 20
->>>>>>> 8dd6b29 (neuronka ukol final final)
-=======
-test_number = 1
->>>>>>> a086f8f (neuronka final final final)
 for i in range(test_number):
     neural_network.fit(trening_X, trening_Y)
 
@@ -153,14 +134,5 @@ for i in range(test_number):
     print(correct / len(results))
 
     print(confusion_matrix(test_Y, results))
-<<<<<<< HEAD
-<<<<<<< HEAD
     ConfusionMatrixDisplay.from_predictions(test_Y, results)
     plt.show()
-=======
->>>>>>> 8dd6b29 (neuronka ukol final final)
-=======
-    ConfusionMatrixDisplay.from_predictions(test_Y, results)
-    plt.show()
->>>>>>> a086f8f (neuronka final final final)
-
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-neural_network = MLPClassifier(
-#     hidden_layer_sizes=(100, 1, 200),
-#     activation="logistic",
-#     max_iter=400,
-#     verbose=False
-# )
